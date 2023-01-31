540b0864dbc5e0b3b61db27fa8c991e65afecc67
Reshte <107108359+Reshte@users.noreply.github.com>
2023-01-31 11:47:10 +0200

    Update MoviesDetails.jsx

--- src/pages/MoviesDetails.jsx
+++ src/pages/MoviesDetails.jsx
@@ -1,10 +1,10 @@
 import { useParams } from "react-router-dom";
-import { getMovieDetails} from "../components/servises/Fetch"
+// import { getMovieDetails } from "../components/servises/Fetch"
 
 export const MoviesDetails = () => {
     const { movietId } = useParams();
     console.log( movietId)
-    const  movie = getMovieDetails(movietId);
+    // const  movie = getMovieDetails(movietId);
   return <div>Now showing product with id - { movietId}</div>;
 };
 
