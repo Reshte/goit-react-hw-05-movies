81d94d2350a393096f8e5d567c4b97f1b6a5efad
Reshte <107108359+Reshte@users.noreply.github.com>
2023-02-01 14:35:20 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -14,7 +14,7 @@ export function Reviews() {
           setReviews(reviews)
         } catch (error) {
           console.log(error)
-            console.log(",kf,kf")
+          
              }
         })()               
     },[movieId])
