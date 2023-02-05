610bd6f99e478e678d914f63f5c9f8913b1fcfeb
Reshte <107108359+Reshte@users.noreply.github.com>
2023-02-05 17:08:51 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,37 +1,39 @@
  import { useEffect, useState } from "react"
  import { getMovieReviews } from "../servises/Fetch"
  import { useParams } from "react-router-dom";
-
+import {Loader} from 'components/Loader/Loader'
 export const Reviews = () => {   
     const { movieId } = useParams();
     const [reviews, setReviews] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         (async function () {
+            setLoading(true)
         try {
-          const reviews = await getMovieReviews(movieId)
-          setReviews(reviews)
-        } catch (error) {
+            const reviews = await getMovieReviews(movieId)
+            if (reviews.length > 0) { setReviews(reviews) } else {setError(true)}
+                            } catch (error) {
           console.log(error)
-          
-             }
-        })()               
+            }
+            setLoading(false)
+        })()  
+        
     },[movieId])
 
     return (
         <div>
-            <ul>
-
-                {reviews.length > 0 ? (reviews.map(({author,id,content})=>{return  <li key={id}>
-                        <p>{author}</p>
-                        <p>{content}</p>
-                    </li>                   
+            {loading && <Loader />}
+            {error && <p>We don`t have any reviews for this movie</p>}
+             {! error &&  <ul>         
+               { reviews.map(({author,id,content})=>{return <li key={id}>
+                <p>{author}</p>
+                <p>{content}</p>
+                </li>                   
                 })
-      ) : (
-        <p>We don`t have any reviews for this movie</p>
-      )}
-                
-            </ul>
+                 }
+            </ul>}
         </div>
     )
 }
